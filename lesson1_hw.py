8320d5c128cb23968c663b56ec473a7738168df9
Denis Lezin <46822594+DenisLezin@users.noreply.github.com>
2019-10-17 01:29:38 +0300

    getting github repos for user and save it to file

--- lesson1_hw.py
+++ lesson1_hw.py
@@ -1,8 +1,9 @@
 import requests
+import json
 from pprint import pprint
 
 user = 'user'
-token = 'token'
+token = 'password'
 
 def get_github_repos(user, token):
     repos_url = requests.get('https://api.github.com/user', auth=(user, token)).json()['repos_url']
@@ -10,4 +11,7 @@ def get_github_repos(user, token):
     return [i['name'] for i in repos]
 
 
-pprint(get_github_repos(user, token))
+with open('github_repos.json', 'w') as f:
+    f.write(json.dumps(get_github_repos(user, token)))
+
+# pprint(get_github_repos(user, token))
