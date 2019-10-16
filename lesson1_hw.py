c87659c2996dfad1bd0af8d9bacee8d8f1d97b06
Denis Lezin <46822594+DenisLezin@users.noreply.github.com>
2019-10-17 00:44:57 +0300

    getting github repos for user

--- lesson1_hw.py
+++ lesson1_hw.py
@@ -1,8 +1,8 @@
 import requests
 from pprint import pprint
 
-user = 'DenisLezin'
-token = 'b15165f06e8c2578be0986e9a5f36b972a025bb0'
+user = 'user'
+token = 'token'
 
 def get_github_repos(user, token):
     repos_url = requests.get('https://api.github.com/user', auth=(user, token)).json()['repos_url']
