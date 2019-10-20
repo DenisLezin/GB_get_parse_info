d7f711325f0c6bcb44d70f97978a001a8836969f
Denis Lezin <46822594+DenisLezin@users.noreply.github.com>
2019-10-21 02:05:18 +0300

    get info from HH

--- lesson2_hw.py
+++ lesson2_hw.py
@@ -1,48 +1,84 @@
 from bs4 import BeautifulSoup as bs
+import re
 import requests
 from pprint import pprint
+import time
 
+def get_compensation_hh(vacancy_compensation):
+    vacancy_compensation = vacancy_compensation.getText().replace('\xa0', '')
+    currency = re.findall(r' \D+', vacancy_compensation)[0]
+    if 'от' in vacancy_compensation:
+        vacancy_compensation_min = re.findall(r'\d+', vacancy_compensation)[0]
+        vacancy_compensation_max = 'Undefined'
+    elif 'до' in vacancy_compensation:
+        vacancy_compensation_min = 'Undefined'
+        vacancy_compensation_max = re.findall(r'\d+', vacancy_compensation)[0]
+    else:
+        vacancy_compensation_min = re.findall(r'\d+', vacancy_compensation)[0]
+        vacancy_compensation_max = re.findall(r'\d+', vacancy_compensation)[1]
+    return [vacancy_compensation_min, vacancy_compensation_max, currency]
 
-main_link = 'https://www.kinopoisk.ru'
-suffix = '/afisha/new/city/1/'
+# Parse HH
 
-headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
+main_link = 'https://hh.ru/search/vacancy'
+vacancy = 'Python junior'
+number_of_pages = 2
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
+suffix = r'/?'+r'text=' + vacancy.replace(' ', '+')
 
 # parsed = requests.get(main_link+suffix, headers=headers).text
-with open('site_content.txt', 'r', encoding='utf-8') as f:
-    parsed = bs(f.read(), 'lxml')
+# with open('hh_content.txt', 'r', encoding='utf-8') as f:
+#    parsed = bs(f.read(), 'lxml')
 
-films_block = parsed.find('div', {'class': 'filmsListNew'})
-films_list = films_block.findChildren(recursive=False)
+result = []
 
-films = []
+for i in range(number_of_pages):
+    parsed = bs(requests.get(main_link+suffix, headers=headers).text, 'lxml')
+    vacancy_block = parsed.find('div', {'class': 'vacancy-serp'})
+    vacancy_list = vacancy_block.find_all('div', {'class': 'vacancy-serp-item'})
 
-for film in films_list:
-    film_data = {}
-    film_info = film.find('div', {'class': 'name'}).findChild()
+    for vac in vacancy_list:
+        vacancy_data = {}
 
-    film_name = film_info.getText()
-    film_link = main_link+film_info['href']
-    genre = film.find_all('div', {'class': 'gray'})[1].getText().replace(' ', '')[9:]
-    rating = film.find('span', {'class': ['rating_ball_green', 'rating_ball_red', 'rating_ball_grey']})
-    rating = 0 if not rating else rating.getText()
-    # print('-' * 200, film_name, film_link, genre, rating, sep='\n')
-    film_data['name'] = film_name
-    film_data['genre'] = genre
-    film_data['rating'] = rating
-    film_data['link'] = film_link
-    films.append(film_data)
+        vacancy_name = vac.find('a').getText()
+        vacancy_link = vac.find('a')['href']
+        vacancy_compensation = vac.find('div', {'class': 'vacancy-serp-item__compensation'})
+        if not vacancy_compensation:
+            vacancy_compensation = ['Undefined', 'Undefined', '']
+        else:
+            vacancy_compensation = get_compensation_hh(vacancy_compensation)
 
-pprint(films)
+        vacancy_data['vacancy'] = vacancy_name
+        vacancy_data['compensation_min'] = str(vacancy_compensation[0]) + vacancy_compensation[2]
+        vacancy_data['compensation_max'] = str(vacancy_compensation[1]) + vacancy_compensation[2]
+        vacancy_data['link'] = vacancy_link
+
+        result.append(vacancy_data)
+
+    suffix = parsed.find('a', {'data-qa': 'pager-next'})['href']
+    if not suffix:
+        break
+    else:
+        suffix = suffix[suffix.find('?'):]
+    time.sleep(1)
+
+
+pprint(result)
 
 
 
-
-
-
-
-
-
-
-# with open('site_content.txt', 'w', encoding='utf-8') as f:
-#     f.write(requests.get(main_link+suffix, headers=headers).text)
+# for i in result:
+#     print(i['compensation'])
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# with open('hh_content.txt', 'w', encoding='utf-8') as f:
+#     f.write(requests.get(main_link, headers=headers, params=params).text)
