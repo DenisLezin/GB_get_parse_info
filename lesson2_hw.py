b6344ec6ffcd196d31b3328cfaf18055f36ceaa7
Denis Lezin <46822594+DenisLezin@users.noreply.github.com>
2019-10-28 01:24:34 +0300

    add functions (updated lesson2_hw.py): 1. request superjob 2. make one function to get info from different sites 3. bs commands placed in dict, which translated to main function

--- lesson2_hw.py
+++ lesson2_hw.py
@@ -11,10 +11,66 @@ def request_to_file(file, link, headers, params):
         f.write(requests.get(link, headers=headers, params=params).text)
 
 
-def get_salary_hh(vacancy_salary):
+def get_vacancies(vacancy, number_of_pages, main_link, headers, test=True):
+
+    request_params = get_params(main_link)
+
+    suffix = eval(request_params['suffix'])
+
+    if test:
+        with open(request_params['test_file'], 'r', encoding='utf-8') as f:
+            parsed = bs(f.read(), 'lxml')
+        number_of_pages = 1
+
+    result = []
+
+    for i in range(number_of_pages):
+        parsed = parsed if test else bs(requests.get(main_link+suffix, headers=headers).text, 'lxml')
+        vacancy_block = eval(request_params['vacancy_block'])
+        vacancy_list = eval(request_params['vacancy_list'])
+
+        for vac in vacancy_list:
+            vacancy_data = {}
+
+            vacancy_name = eval(request_params['name'])
+            vacancy_city = eval(request_params['city'])
+            vacancy_link = eval(request_params['link'])
+            vacancy_salary = eval(request_params['salary'])
+            vacancy_salary = get_salary(vacancy_salary)
+
+            vacancy_data['vacancy'] = vacancy_name
+            vacancy_data['city'] = vacancy_city
+            vacancy_data['salary_min'] = vacancy_salary[0]
+            vacancy_data['salary_max'] = vacancy_salary[1]
+            vacancy_data['salary_currency'] = vacancy_salary[2]
+            vacancy_data['link'] = vacancy_link
+            vacancy_data['site'] = main_link
+
+            result.append(vacancy_data)
+
+        suffix = eval(request_params['next_page_suffix'])
+        if not suffix:
+            break
+        else:
+            suffix = suffix['href']
+        time.sleep(randint(1, 3))
+    return result
+
+
+def get_salary(vacancy_salary):
+    if not vacancy_salary or 'По договорённости' in vacancy_salary:
+        return [None, None, None]
+
     vacancy_salary = vacancy_salary.getText().replace('\xa0', '')
-    currency = re.search(r' \D+', vacancy_salary).group(0).replace('.', '').strip()
-    if 'от' in vacancy_salary:
+
+    currency = re.search(r' \D+', vacancy_salary)
+
+    if currency:
+        currency = currency.group(0).replace('.', '').strip()
+    else:
+        currency = vacancy_salary[-1]
+
+    if 'от' in vacancy_salary or '-' not in vacancy_salary:
         vacancy_salary_min = re.findall(r'\d+', vacancy_salary)[0]
         vacancy_salary_max = None
     elif 'до' in vacancy_salary:
@@ -27,130 +83,103 @@ def get_salary_hh(vacancy_salary):
             float(vacancy_salary_max) if vacancy_salary_max else None,
             currency]
 
-def get_vacancies_hh(vacancy, number_of_pages, test=True):
-    # Parse HH
 
-    main_link = 'https://hh.ru/search/vacancy'
+def get_params(main_link):
+    request_params_hh = {"suffix": "r'/search/vacancy/?'+r'text=' + vacancy.replace(' ', '+')",
+                         "vacancy_block": "parsed.find('div', {'class': 'vacancy-serp'})",
+                         "vacancy_list": "vacancy_block.find_all('div', {'class': 'vacancy-serp-item'})",
+                         "name": "vac.find('a').getText()",
+                         "city": "re.search(r'^[\w-]+', vac.find('span', {'class': 'vacancy-serp-item__meta-info'})."
+                                 "getText()).group(0)",
+                         "link": "vac.find('a')['href']",
+                         "salary": "vac.find('div', {'class': 'vacancy-serp-item__compensation'})",
+                         "next_page_suffix": "parsed.find('a', {'data-qa': 'pager-next'})",
+                         "test_file": "hh_content.txt"}
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
-    suffix = r'/?'+r'text=' + vacancy.replace(' ', '+')
+    request_params_superjob = {"suffix": "'/vacancy/search/?keywords={}'.format(vacancy.replace(' ', '%20'))",
+                               "vacancy_block": "parsed.find('div', {'class': '_1ID8B'})",
+                               "vacancy_list": "vacancy_block.find_all('div', {'class': "
+                                               "'_3zucV _2GPIV f-test-vacancy-item i6-sc _3VcZr'})",
+                               "name": "vac.find('div', {'class': '_3mfro CuJz5 PlM3e _2JVkc _3LJqf'}).getText()",
+                               "city": "re.search(r'• [\w-]+,?', vac.find('div', {'class': '_2XXYS _2cxK3'})."
+                                       "getText()).group(0)[2:].replace(',', '')",
+                               "link": "main_link + vac.find('a')['href']",
+                               "salary": "vac.find('span', {'class': "
+                                         "'_3mfro _2Wp8I f-test-text-company-item-salary PlM3e _2JVkc _2VHxz'})",
+                               "next_page_suffix": "parsed.find('a', {'class': "
+                                                   "'icMQ_ _1_Cht _3ze9n f-test-button-dalshe f-test-link-dalshe'})",
+                               "test_file": "superjob_content.txt"}
 
-    if test:
-        with open('hh_content.txt', 'r', encoding='utf-8') as f:
-            parsed = bs(f.read(), 'lxml')
-        number_of_pages = 1
-    else:
-        parsed = requests.get(main_link+suffix, headers=headers).text
-
-    result = []
-
-    for i in range(number_of_pages):
-        parsed = parsed if test else bs(requests.get(main_link+suffix, headers=headers).text, 'lxml')
-        vacancy_block = parsed.find('div', {'class': 'vacancy-serp'})
-        vacancy_list = vacancy_block.find_all('div', {'class': 'vacancy-serp-item'})
-
-        for vac in vacancy_list:
-            vacancy_data = {}
-
-            vacancy_name = vac.find('a').getText()
-            vacancy_city = re.search(r'^[\w-]+', vac.find('span', {'class': 'vacancy-serp-item__meta-info'}).getText()).group(0)
-            vacancy_link = vac.find('a')['href']
-            vacancy_salary = vac.find('div', {'class': 'vacancy-serp-item__compensation'})
-            if not vacancy_salary:
-                vacancy_salary = [None, None, None]
-            else:
-                vacancy_salary = get_salary_hh(vacancy_salary)
-
-            vacancy_data['vacancy'] = vacancy_name
-            vacancy_data['city'] = vacancy_city
-            vacancy_data['salary_min'] = vacancy_salary[0]
-            vacancy_data['salary_max'] = vacancy_salary[1]
-            vacancy_data['salary_currency'] = vacancy_salary[2]
-            vacancy_data['link'] = vacancy_link
-
-            result.append(vacancy_data)
-
-        suffix = parsed.find('a', {'data-qa': 'pager-next'})['href']
-        if not suffix:
-            break
-        else:
-            suffix = suffix[suffix.find('?'):]
-        time.sleep(randint(1, 3))
-    return result
+    if main_link == 'https://hh.ru':
+        return request_params_hh
+    elif main_link == 'https://www.superjob.ru':
+        return request_params_superjob
 
 
-def get_vacancies_superjob(vacancy, number_of_pages, test=True):
-    # Parse SuperJob
+# vacancy = 'Аналитик'
+# number_of_pages = 2
+#
+# main_link_superjob = 'https://www.superjob.ru'
+# main_link_hh = 'https://hh.ru'
+#
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+#                          ' Chrome/77.0.3865.120 Safari/537.36'}
+#
+#
+# res = get_vacancies(vacancy, number_of_pages, main_link_hh, headers, test=False)
+#
+# pprint(res)
 
-    main_link = 'https://www.superjob.ru'
-    suffix = f'/vacancy/search/?keywords=' + vacancy.replace(' ', '%20')
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                             ' Chrome/77.0.3865.120 Safari/537.36'}
-    params = {'keywords': vacancy}
-
-    if test:
-        with open('superjob_content.txt', 'r', encoding='utf-8') as f:
-            parsed = bs(f.read(), 'lxml')
-        number_of_pages = 1
-    # else:
-    #     parsed = requests.get(main_link+suffix, headers=headers).text
-
-    result = []
-
-    for i in range(number_of_pages):
-        parsed = parsed if test else bs(requests.get(main_link+suffix, headers=headers).text, 'lxml')
-        vacancy_block = parsed.find('div', {'class': '_1ID8B'})  # parsed.find_all('div', {'class': '_1Ttd8 _2CsQi'})[0]
-        vacancy_list = vacancy_block.find_all('div', {'class': '_3zucV _2GPIV f-test-vacancy-item i6-sc _3VcZr'})
-
-        for vac in vacancy_list:
-            vacancy_data = {}
-
-            vacancy_name = eval(request_params_superjob['vacancy'])  # vac.find('div', {'class': '_3mfro CuJz5 PlM3e _2JVkc _3LJqf'}).getText()
-            vacancy_city = eval(request_params_superjob['city'])  # re.search(r'• [\w-]+,?', vac.find('div', {'class': '_2XXYS _2cxK3'}).getText()).group(0)[2:].replace(',', '')
-            vacancy_link = eval(request_params_superjob['link'])  # main_link + vac.find('a')['href']
-            vacancy_salary = eval(request_params_superjob['salary'])  # vac.find('span', {'class': '_3mfro _2Wp8I f-test-text-company-item-salary PlM3e _2JVkc _2VHxz'}).getText()   # .replace('\xa0', '')
-           # if not vacancy_salary:
-           #     vacancy_salary = [None, None, None]
-           # else:
-           #     vacancy_salary = get_salary_hh(vacancy_salary)
-
-            vacancy_data['vacancy'] = vacancy_name
-            vacancy_data['city'] = vacancy_city
-           # vacancy_data['salary_min'] = vacancy_salary[0]
-           # vacancy_data['salary_max'] = vacancy_salary[1]
-           # vacancy_data['salary_currency'] = vacancy_salary[2]
-            vacancy_data['link'] = vacancy_link
-            vacancy_data['salary'] = vacancy_salary
-
-            result.append(vacancy_data)
-
-        suffix = parsed.find('a', {'class': 'icMQ_ _1_Cht _3ze9n f-test-button-dalshe f-test-link-dalshe'})
-        if not suffix:
-            break
-        else:
-            suffix = suffix['href']
-        time.sleep(randint(1, 3))
-    return result
-
-request_params_superjob = {"vacancy": "vac.find('div', {'class': '_3mfro CuJz5 PlM3e _2JVkc _3LJqf'}).getText()",
-                           "city": "re.search(r'• [\w-]+,?', vac.find('div', {'class': '_2XXYS _2cxK3'})."
-                                   "getText()).group(0)[2:].replace(',', '')",
-                           "link": "main_link + vac.find('a')['href']",
-                           "salary": "vac.find('span', {'class': "
-                                     "'_3mfro _2Wp8I f-test-text-company-item-salary PlM3e _2JVkc _2VHxz'}).getText()"}
-
-vacancy = 'Аналитик'
-number_of_pages = 1
-
-main_link = 'https://www.superjob.ru'
-suffix = f'/vacancy/search/?keywords=' + vacancy.replace(' ', '%20')
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                         ' Chrome/77.0.3865.120 Safari/537.36'}
-params = {'keywords': vacancy}
-
-res = get_vacancies_superjob(vacancy, number_of_pages, test=True)
-
-pprint(res)
-# request_to_file('superjob_content.txt', main_link+suffix, headers, None)
+# # request_to_file('superjob_content.txt', main_link+suffix, headers, None)
+#
+# def get_vacancies_hh(vacancy, number_of_pages, test=True):
+#     # Parse HH
+#
+#     main_link = 'https://hh.ru/search/vacancy'
+#
+#     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
+#     suffix = r'/?'+r'text=' + vacancy.replace(' ', '+')
+#
+#     if test:
+#         with open('hh_content.txt', 'r', encoding='utf-8') as f:
+#             parsed = bs(f.read(), 'lxml')
+#         number_of_pages = 1
+#     else:
+#         parsed = requests.get(main_link+suffix, headers=headers).text
+#
+#     result = []
+#
+#     for i in range(number_of_pages):
+#         parsed = parsed if test else bs(requests.get(main_link+suffix, headers=headers).text, 'lxml')
+#         vacancy_block = parsed.find('div', {'class': 'vacancy-serp'})
+#         vacancy_list = vacancy_block.find_all('div', {'class': 'vacancy-serp-item'})
+#
+#         for vac in vacancy_list:
+#             vacancy_data = {}
+#
+#             vacancy_name = vac.find('a').getText()
+#             vacancy_city = re.search(r'^[\w-]+', vac.find('span', {'class': 'vacancy-serp-item__meta-info'}).getText()).group(0)
+#             vacancy_link = vac.find('a')['href']
+#             vacancy_salary = vac.find('div', {'class': 'vacancy-serp-item__compensation'})
+#             if not vacancy_salary:
+#                 vacancy_salary = [None, None, None]
+#             else:
+#                 vacancy_salary = get_salary_hh(vacancy_salary, vacancy_name)
+#
+#             vacancy_data['vacancy'] = vacancy_name
+#             vacancy_data['city'] = vacancy_city
+#             vacancy_data['salary_min'] = vacancy_salary[0]
+#             vacancy_data['salary_max'] = vacancy_salary[1]
+#             vacancy_data['salary_currency'] = vacancy_salary[2]
+#             vacancy_data['link'] = vacancy_link
+#
+#             result.append(vacancy_data)
+#
+#         suffix = parsed.find('a', {'data-qa': 'pager-next'})['href']
+#         if not suffix:
+#             break
+#         else:
+#             suffix = suffix[suffix.find('?'):]
+#         time.sleep(randint(1, 3))
+#     return result
--- lesson3_hw.py
+++ lesson3_hw.py
@@ -1,4 +1,4 @@
-from lesson2_hw import get_vacancies_hh
+from lesson2_hw import get_vacancies
 from pprint import pprint
 from pymongo import MongoClient
 
@@ -10,15 +10,20 @@ vacancy = 'Python junior'
 number_of_pages = 5
 min_salary = 60000
 
-def fill_db(vacancy, number_of_pages, test=True):
+main_link = ['https://www.superjob.ru', 'https://hh.ru']
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                         ' Chrome/77.0.3865.120 Safari/537.36'}
+
+def fill_db(vacancy, number_of_pages, main_link, headers, test=True):
     hh.delete_many({})
-    hh.insert_many(get_vacancies_hh(vacancy, number_of_pages, test))
+    for link in main_link:
+        hh.insert_many(get_vacancies(vacancy, number_of_pages, link, headers, test))
 
 def get_vacancies_min_salary(salary):
     return hh.find({'salary_min':  {'$gte': salary}})
 
 
-fill_db(vacancy, number_of_pages, test=False)
+fill_db(vacancy, number_of_pages, main_link, headers, test=False)
 objects = get_vacancies_min_salary(min_salary)
 # objects = hh.find()
 
