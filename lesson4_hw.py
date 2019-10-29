aa0fc99bf6693b93177bbb2dde4f151ab1554972
Denis Lezin <46822594+DenisLezin@users.noreply.github.com>
2019-10-30 00:13:53 +0300

    add functions:
    get news from mail.ru

---
+++ lesson4_hw.py
@@ -0,0 +1,41 @@
+from lxml import html
+import requests
+from pprint import pprint
+from numpy import datetime64
+
+
+def get_mail_ru_news():
+    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
+
+    mail_ru_requests = {'paths': '//div[@class="news-item o-media news-item_media news-item_main"]/a/@href |'
+                                 '//div[@class="news-item__inner"]/a[not (@class)]/@href',
+                        'news': '//h3[1]/text() |'
+                                '//div[@class="news-item__inner"]/a[not (@class)]/text()',
+                        'date': '//span[contains(@class, "js-ago")]/@datetime'}
+
+    req = requests.get('https://mail.ru/', headers=header)
+
+    ml_root = html.fromstring(req.text)
+
+    ml_paths = ml_root.xpath(mail_ru_requests['paths'])
+
+    ml_news = ml_root.xpath(mail_ru_requests['news'])
+
+    items = []
+    for i in range(len(ml_paths)):
+        item = {'site': 'Mail.ru',
+                'news': ml_news[i].replace('\xa0', ' '),
+                'path': ml_paths[i],
+                'date': datetime64(html.fromstring(requests.get(ml_paths[i], headers=header).text).xpath(
+                    mail_ru_requests['date'])[0][:19])}
+        items.append(item)
+    return items
+
+
+
+pprint(get_mail_ru_news())
+
+
+# ml_root.xpath("//div[@class='news-item o-media news-item_media news-item_main']/a/@href")
+# '//span[@class = "note__text breadcrumbs__tdateext js-ago"]/@datetime'
--- mongodb.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-
-client = MongoClient('localhost', 27017)
-db = client['test_mdb']
-
-db.users
