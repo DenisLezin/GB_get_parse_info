e0ca04ed0ec4101dee2133cd1c5e6473893f8d5d
Denis Lezin <46822594+DenisLezin@users.noreply.github.com>
2019-10-31 00:07:50 +0300

    get err Forbidden by robots.txt

--- getvacancies/spiders/hhru.py
+++ getvacancies/spiders/hhru.py
@@ -4,8 +4,8 @@ import scrapy
 
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
-    allowed_domains = ['https://www.superjob.ru/']
-    start_urls = ['https://www.superjob.ru/vacancy/search/?keywords=python']
+    allowed_domains = ['https://hh.ru/']
+    start_urls = ['https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=python']
 
     def parse(self, response):
         print(response)
